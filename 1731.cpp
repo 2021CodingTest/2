d5372021f4eb9d82ea03efc6f880a92ad534af56
JungHeeSoo <73633272+heesoo01@users.noreply.github.com>
2021-07-29 11:56:38 +0900

    Create 1731.cpp

---
+++ 1731.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    long int arr[N]={0,};
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    if((arr[1]-arr[0])==(arr[2]-arr[1])){
+        int Q=arr[1]-arr[0];
+        int res=arr[N-1]+Q;
+        //Q*(N-1) + P
+        cout << res;
+        exit(0);
+    }
+    if((arr[1]/arr[0])==(arr[2]/arr[1])){
+        int Q=arr[1]/arr[0];
+        int res=arr[N-1]*(Q);
+        //Q^(N-1) * P
+        cout << res;
+    }
+}
