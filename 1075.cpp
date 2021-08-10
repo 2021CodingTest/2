6a3a0216974181cf9a2ab0d48ba6252aeacc2d38
JungHeeSoo <73633272+heesoo01@users.noreply.github.com>
2021-08-10 23:33:46 +0900

    Create 1075.cpp

---
+++ 1075.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, F, value;
+	int num1;
+	cin >> N >> F;
+	num1 = (N / 100) * 100;
+	value = (num1 / F) * F;
+	while (value < num1) {
+		value += F;
+	}
+	value %= 100;
+	if (value < 10) {
+		cout << "0" << value << endl;
+	}
+	else {
+		cout << value << endl;
+	}
+	
+}
